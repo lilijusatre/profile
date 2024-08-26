96a2c5d8918b3f8384ca4dda1b7d1228d2f0d490
Lilia <lilijusatre@gmail.com>
2024-08-25 22:40:40 -0600

    ajuste de estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,11 @@ const icons = document.querySelectorAll("#icons");
 icons.forEach((icon) => {
   icon.addEventListener("mouseover", () => {
     // Agregamos una clase o modificamos el estilo directamente
-    icon.style.transform = "scale(1.2)";
+    // icon.style.transform = "scale(1.2)";
   });
   icon.addEventListener("mouseout", () => {
     // Restauramos la clase o estilo original
-    icon.style.transform = "scale(1)";
+    // icon.style.transform = "scale(1)";
   });
 });
 const icons2 = document.querySelectorAll(".contact-icon");
@@ -34,11 +34,11 @@ const cards = document.querySelectorAll(".project_card");
 cards.forEach((card) => {
   card.addEventListener("mouseover", () => {
     // Agregamos una clase o modificamos el estilo directamente
-    card.style.transform = "scale(1.2)";
+    // card.style.transform = "scale(1.2)";
   });
   card.addEventListener("mouseout", () => {
     // Restauramos la clase o estilo original
-    card.style.transform = "scale(1)";
+    // card.style.transform = "scale(1)";
   });
 });
 const emailButton = document.querySelector("#btn-email");
